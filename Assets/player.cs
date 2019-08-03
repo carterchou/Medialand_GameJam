1c545de4dfed9db0a6b18e82cc92f032d6570be7
carterchou <carter870808@gmail.com>
2019-08-03 13:59:54 +0800

    controller & fungus test

---
+++ Assets/player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(UnityEngine.Collision hit)
+    {
+        //Debug.Log("碰到");
+        if (hit.gameObject.tag == "cube")
+        {
+            Flowchart.BroadcastFungusMessage("hitCube");
+        }
+
+    }
+
+}
