6912c79f60737c0fb7ca259f96e69dcbc817d0b7
vvbest2012 <vvbest2012@gmail.com>
2019-08-04 11:11:47 +0800

    Notification function modify fungus

---
+++ Assets/scriot/Core/NotificationKeys.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NotificationKeys
+{
+
+    ////MapWorld
+    //public const string EnteringTheMapWorld = "EnteringTheMapWorld";
+    //public const string LeavingTheMapWorld = "LeavingTheMapWorld";
+    //public const string ReceivingMapUIdata = "ReceivingMapUIdata";
+    //public const string GetBackLobbyState = "GetBackLobbyState";
+    //public const string Test = "Test";
+
+    ////EventTrigger
+    //public const string EventOnTriggered = "EventOnTriggered";
+    //public const string HasNextEvent = "HasNextEvent";
+
+    ////Player Controller
+    //public const string PlayerTurnLeft = "PlayerTurnLeft";
+    //public const string PlayerTurnRight = "PlayerTurnRight";
+    //public const string PlayerControllerOn = "PlayerControllerOn";
+    //public const string PlayerControllerOff = "PlayerControllerOff";
+}
