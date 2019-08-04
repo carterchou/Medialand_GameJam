44fe2d7dfc59d4cff5e5e42dba86cecce10f67b6
AAA <105703008@nccu.edu.tw>
2019-08-04 13:50:38 +0800

    室外腳步聲

--- Assets/scriot/player.cs
+++ Assets/scriot/player.cs
@@ -13,6 +13,7 @@ public class player : MonoBehaviour
     public GameObject[] weapon;
     public vThirdPersonController v_con ;
     public bool hasWeapon = false;
+    private int ground_kind = 0;
 
     public AudioClip[] SE;// 0 is attack
     AudioSource audiosource;
@@ -52,6 +53,17 @@ public class player : MonoBehaviour
             Flowchart.BroadcastFungusMessage("hitCube");
         }
 
+    }
+    void OnCollisionStay(UnityEngine.Collision ground)
+    {
+        if (ground.gameObject.tag == "soil")
+        {
+            ground_kind = 0;
+        }
+        else if(ground.gameObject.tag == "lawn")
+        {
+            ground_kind = 1;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -112,7 +124,7 @@ public class player : MonoBehaviour
     }
     public void walkSE()
     {
-        audiosource.PlayOneShot(SE[Random.Range(1, 7)]);
+        audiosource.PlayOneShot(SE[ground_kind*10 + Random.Range(1, 11)]);
 
     }
     public void getWeapon()
