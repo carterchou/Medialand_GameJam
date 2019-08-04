5dd1dc37c48e984b42506527719a29d3fe18ff8e
vvbest2012 <vvbest2012@gmail.com>
2019-08-04 12:55:37 +0800

    teleport function

--- Assets/scriot/Core/NotificationKeys.cs
+++ Assets/scriot/Core/NotificationKeys.cs
@@ -5,20 +5,6 @@ using UnityEngine;
 public static class NotificationKeys
 {
 
-    ////MapWorld
-    //public const string EnteringTheMapWorld = "EnteringTheMapWorld";
-    //public const string LeavingTheMapWorld = "LeavingTheMapWorld";
-    //public const string ReceivingMapUIdata = "ReceivingMapUIdata";
-    //public const string GetBackLobbyState = "GetBackLobbyState";
-    //public const string Test = "Test";
-
-    ////EventTrigger
-    //public const string EventOnTriggered = "EventOnTriggered";
-    //public const string HasNextEvent = "HasNextEvent";
-
-    ////Player Controller
-    //public const string PlayerTurnLeft = "PlayerTurnLeft";
-    //public const string PlayerTurnRight = "PlayerTurnRight";
-    //public const string PlayerControllerOn = "PlayerControllerOn";
-    //public const string PlayerControllerOff = "PlayerControllerOff";
+    //Game
+    public const string TeleportTo = "TeleportTo";
 }
---
+++ Assets/scriot/Test/WilliamTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WilliamTest : MonoBehaviour
+{
+    public GameObject test1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("WilliamTest OnTriggerEnter");
+        NotificationCenter.Default.Post(this,NotificationKeys.TeleportTo, TeleportNum.Test1);
+    }
+}
